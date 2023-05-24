d36ad4d1c12e5430171c794d64d9e7097f0638a9
Dmitry Berdnikov <berdnikovdmitry@mail.ru>
2023-05-24 23:53:56 +0500

    add basic logic with failed test

--- src/game.test.ts
+++ src/game.test.ts
@@ -24,3 +24,80 @@ describe('players signs', () => {
 		expect(game.getAiSign()).toBe(SIGN_X)
 	})
 })
+
+describe('game', () => {
+	// TODO: Test fails because of ai move is random, it should be mocked
+	it('should have correct signs in the game board', () => {
+		const game = createGame()
+
+		game.move(0, 1)
+		let firstRoundAiSignsCount = 0
+
+		for (let x = 0; x < 3; x += 1) {
+			for (let y = 0; y < 3; y += 1) {
+				if (game.hasAiSign(x, y)) {
+					firstRoundAiSignsCount += 1
+				}
+
+				if (x === 0 && y === 1) {
+					expect(game.hasPlayerSign(x, y)).toBeTruthy()
+
+					continue
+				}
+
+				expect(game.hasPlayerSign(x, y)).toBeFalsy()
+			}
+		}
+
+		expect(firstRoundAiSignsCount).toBe(1)
+		expect(game.getWinner()).toBeNull()
+
+		game.move(1, 1)
+		let secondRoundAiSignsCount = 0
+
+		for (let x = 0; x < 3; x += 1) {
+			for (let y = 0; y < 3; y += 1) {
+				if (game.hasAiSign(x, y)) {
+					secondRoundAiSignsCount += 1
+				}
+
+				if ((x === 0 && y === 1) || (x === 1 && y === 1)) {
+					expect(game.hasPlayerSign(x, y)).toBeTruthy()
+
+					continue
+				}
+
+				expect(game.hasPlayerSign(x, y)).toBeFalsy()
+			}
+		}
+
+		expect(secondRoundAiSignsCount).toBe(2)
+		expect(game.getWinner()).toBeNull()
+
+		game.move(2, 1)
+		let thirdRoundAiSignsCount = 0
+
+		for (let x = 0; x < 3; x += 1) {
+			for (let y = 0; y < 3; y += 1) {
+				if (game.hasAiSign(x, y)) {
+					thirdRoundAiSignsCount += 1
+				}
+
+				if (
+					(x === 0 && y === 1) ||
+					(x === 1 && y === 1) ||
+					(x === 2 && y === 1)
+				) {
+					expect(game.hasPlayerSign(x, y)).toBeTruthy()
+
+					continue
+				}
+
+				expect(game.hasPlayerSign(x, y)).toBeFalsy()
+			}
+		}
+
+		expect(thirdRoundAiSignsCount).toBe(3)
+		expect(game.getWinner()).toBe('player')
+	})
+})
--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,10 @@
 export const SIGN_X = 'x'
 export const SIGN_O = 'o'
 
+export const getRandomNumber = (min: number, max: number): number => {
+	return min + Math.floor(Math.random() * (max - min + 1))
+}
+
 type GameSign = typeof SIGN_X | typeof SIGN_O
 
 type GameParams = {
@@ -10,17 +14,58 @@ type GameParams = {
 type Game = {
 	getPlayerSign: () => GameSign
 	getAiSign: () => GameSign
+	hasPlayerSign: (x: number, y: number) => boolean
+	hasAiSign: (x: number, y: number) => boolean
+	move: (x: number, y: number) => void
+	getWinner: () => null | 'player' | 'ai'
 }
 
 export const createGame = (params?: GameParams): Game => {
 	const config = params ?? {}
 	const { playerSign = 'x' } = config
+	const board: Array<Array<null | GameSign>> = [
+		[null, null, null],
+		[null, null, null],
+		[null, null, null],
+	]
+
+	const getWinner = () => null
 
 	const getPlayerSign = () => playerSign
+
 	const getAiSign = () => (playerSign === 'x' ? 'o' : 'x')
+
+	const hasPlayerSign = (x: number, y: number) =>
+		board[x][y] === getPlayerSign()
+
+	const hasAiSign = (x: number, y: number) => board[x][y] === getAiSign()
+
+	const moveAi = () => {
+		const x = getRandomNumber(0, board.length - 1)
+		const y = getRandomNumber(0, board[0].length - 1)
+
+		if (board[x][y] !== null) {
+			moveAi()
+
+			return
+		}
+
+		board[x][y] = getAiSign()
+	}
+
+	const movePlayer = (x: number, y: number) => {
+		if (!board[x][y]) {
+			board[x][y] = getPlayerSign()
+			moveAi()
+		}
+	}
 
 	return {
 		getPlayerSign,
 		getAiSign,
+		hasPlayerSign,
+		hasAiSign,
+		move: movePlayer,
+		getWinner,
 	}
 }
